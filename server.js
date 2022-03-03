dc1338739d62c0c2c6306f712ca7d9f247ef9696
Wys2 <noahgaming951@gmail.com>
2022-03-03 19:49:11 +0100

    Language system

--- server.js
+++ server.js
@@ -2,39 +2,32 @@
 
 // Startup
 console.clear()
-
 const log = console.log;
 const chalk = require('chalk');
 const package = require("./package.json")
 
 log(chalk.inverse(`${package.name} V.${package.version}, by ${package.author}.`))
-log(chalk.inverse("Loading Consts"))
 
 // Constantes
+log(chalk.inverse("Loading Consts..."))
 const licenseKey = require('nodejs-license-key');
 const fs = require("fs")
 
 // Lang
-log(chalk.inverse("Loading language files"))
-let languageArray;
+log(chalk.inverse("Loading language files..."))
+let languageArray = []
 
 try {
-    fs.readdirSync("./lang").forEach(lng => {
-        let languageNum = 0
-        let languages = fs.readdirSync(`./lang/${lng}/`).filter((file) => file.endsWith(".json"));
-        for (lngs of languages) {
-            let pull = require(`../lang/${lng}/${lngs}`);
-            if (pull) {
-                languageArray.set(lngs, pull);
-                log(chalk.bgGreen(`Language ${lngs} has been loaded !`))
-                languageNum++
-            } else {
-                console.warn(`Language ${lng} couldn't be loaded`);
-                continue;
-            }
+    fs.readdirSync("./lang").forEach(pull => {
+        if(pull.endsWith(".json")){
+            languageArray.push(pull)
+        } else {
+            log(chalk.bgRed(`${pull} isn't a json file!`))
         }
     })
-    
+    log(chalk.bgGreen(`Loaded ${languageArray.length} languages!`))
 } catch (error) {
     log(chalk.bgRed(error.message))
 }
+
+// Language Selector
